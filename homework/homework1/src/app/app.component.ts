f5848cc6f2e30452f714824e2a3bd4c66f8c427d
Alexander Svistunov <alexsvistunov73@gmail.com>
2018-02-05 08:40:21 +0300

    moved users-array in app-component

--- homework/homework1/src/app/app.component.ts
+++ homework/homework1/src/app/app.component.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public Users: Array<object> = [
+    {
+      name: "Пётр Алексеевич Романов (I)",
+      position: "Царь",
+      avatar: "http://psifactor.info/wp-content/uploads/2016/12/ue0bzh5s.jpeg\n"
+    },
+    {
+      name: "Чапаев Василий Иванович",
+      position: "Ведущий инженер",
+      avatar: "http://uvalibrary.ucoz.net/_pu/0/68577133.jpg"
+    },
+    {
+      name: "Ржевский",
+      position: "Поручик",
+      avatar: "https://im0-tub-ru.yandex.net/i?id=8222ed9fb8dfa69ed8ee271d25456775-l&n=13"
+    }
+  ];
+
   constructor() {}
 
 }
--- homework/homework1/src/app/user-card/user-card.component.ts
+++ homework/homework1/src/app/user-card/user-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
@@ -7,25 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCardComponent implements OnInit {
 
-  public userBackgroundColor = ['rgb(220, 220, 200)', 'rgb(220, 220, 200)', 'rgb(220, 220, 200)'];
+  @Input() userCard;
 
-  public Users = [
-    {
-      name: "Пётр Алексеевич Романов (I)",
-      position: "Царь",
-      avatar: "http://psifactor.info/wp-content/uploads/2016/12/ue0bzh5s.jpeg\n"
-    },
-    {
-      name: "Чапаев Василий Иванович",
-      position: "Ведущий инженер",
-      avatar: "http://uvalibrary.ucoz.net/_pu/0/68577133.jpg"
-    },
-    {
-      name: "Ржевский",
-      position: "Поручик",
-      avatar: "https://im0-tub-ru.yandex.net/i?id=8222ed9fb8dfa69ed8ee271d25456775-l&n=13"
-    }
-  ];
+  public userBackgroundColor = 'rgb(220, 220, 200)';
 
   private randomNumber = () => {
     return Math.round(Math.random() * 255 * 255 * 255);
@@ -33,8 +17,8 @@ export class UserCardComponent implements OnInit {
 
   constructor() { }
 
-  changeColor(index) {
-    this.userBackgroundColor[index] = `#${this.randomNumber().toString(16)}`;
+  changeColor() {
+    this.userBackgroundColor = `#${this.randomNumber().toString(16)}`;
   }
 
   ngOnInit() {
