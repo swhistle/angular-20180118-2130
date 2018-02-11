594ebec180561b44439aeddf67c300b973fcafc8
Alexander Svistunov <alexsvistunov73@gmail.com>
2018-02-12 01:21:43 +0300

    added component sign-up-form with validation and addUser method

---
+++ homework/homework4/alexander.svistunov/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
